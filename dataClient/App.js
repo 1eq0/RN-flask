3ba647ef5ea3dbc883c0f388b23a8ed1438e301d
한은영 <oneeqzero@gmail.com>
2022-04-27 13:28:19 +0900

    reps auto update

--- dataClient/App.js
+++ dataClient/App.js
@@ -5,19 +5,37 @@ import {WebView} from 'react-native-webview';
 
 export default function App() {
 
+  const [reps, setReps] = useState(0);
+
   const handleOnMessage = (e)=>{
     console.log(e.nativeEvent.data);
   };
 
+  const getReps = async () => {
+    try{
+      const response = await fetch("http://localhost:8000/reps");
+      const json = await response.json();
+      setReps(json.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{setInterval(()=>{
+    getReps(),1000
+  })}
+  );
+
   return (
     <View style={styles.container}>
       <WebView
-        source={{uri:"http://localhost:8888/video_feed"}}
+        source={{uri:"http://localhost:8000/video_feed"}}
         onMessage={handleOnMessage}
         javaScriptEnabled={true}
         automaticallyAdjustContentInsets={false}
-        style={{flex:1,width:400}}
+        style={{flex:2,width:400}}
       />
+      <Text style={{flex:1}}>{reps}</Text>
       <StatusBar style="auto" />
     </View>
   );
